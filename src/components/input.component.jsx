0184b34671899aa6c83c70c04cba159042726f6a
emon02 <abdulalimemon11@gmail.com>
2023-12-21 16:48:55 +0600

    complete sign in and sign up page

---
+++ src/components/input.component.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const InputBox = ({ name, type, id, value, placeholder, icon }) => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  return (
+    <div className=" relative mb-4 w-[100%]">
+      <input
+        type={
+          type == "password" ? (passwordVisible ? "text" : "password") : type
+        }
+        name={name}
+        placeholder={placeholder}
+        defaultValue={value}
+        id={id}
+        className="input-box"
+      />
+      <i class={`fi ${icon} input-icon`}></i>
+
+      {type == "password" ? (
+        <i
+          class={`fi ${passwordVisible ? 'fi-rs-eye' : "fi-rr-eye-crossed"}  input-icon left-[auto] right-4 cursor-pointer`}
+          onClick={() => setPasswordVisible((currentVal) => !currentVal)}
+        ></i>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default InputBox;
---
+++ src/pages/userAuthForm.page.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import InputBox from "../components/input.component";
+import googleIcon from "../imgs/google.png";
+import { Link } from "react-router-dom";
+
+const UserAuthForm = ({ type }) => {
+  return (
+    <>
+      <section className="h-cover flex items-center justify-center ">
+        <form className="w-[90%] max-w-[400px]">
+          <h2 className="text-4xl font-gelasio capitalize text-center mb-24">
+            {type == "sign-in" ? "Welcome Back" : "Join us Today"}
+          </h2>
+
+          {type != "sign-in" ? (
+            <InputBox
+              name="fullname"
+              type="text"
+              id=""
+              placeholder="Full Name"
+              icon="fi-rr-user"
+            />
+          ) : (
+            ""
+          )}
+
+          <InputBox
+            name="email"
+            type="email"
+            id=""
+            placeholder="Email"
+            icon="fi-rr-envelope"
+          />
+
+          <InputBox
+            name="password"
+            type="password"
+            id=""
+            placeholder="Password"
+            icon="fi-rr-fingerprint"
+          />
+
+          <button className="btn-dark center mt-14 w-full" type="submit">
+            {type == "sign-in" ? "Sign In" : "Sign Up"}
+          </button>
+
+          <div className=" relative w-full flex items-center gap-2 my-10 uppercase text-black font-bold">
+            <hr className="w-1/2 border-black opacity-10" />
+            <p className="text-black opacity-40">or</p>
+            <hr className="w-1/2 border-black opacity-10" />
+          </div>
+
+          <button className="btn-dark w-full flex items-center justify-center gap-5 ">
+            <img src={googleIcon} alt="continue with google" className="w-5" />
+            continue with google
+          </button>
+
+          {type == "sign-in" ? (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Dont have an account?{" "}
+              <Link to="/signup" className=" underline text-black text-xl ml-1">
+                Join us today.
+              </Link>
+            </p>
+          ) : (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Already a member?{" "}
+              <Link to="/signin" className=" underline text-black text-xl ml-1">
+                Sign in here.
+              </Link>
+            </p>
+          )}
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default UserAuthForm;
